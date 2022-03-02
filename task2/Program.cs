b05b2e917c22469afe75d01b733ddca8ab096b56
maksotik <maksotik@rambler.ru>
2022-03-03 01:02:25 +0300

    Задача 2 удаление второго числа из случайного

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+Random rnd = new Random();
+    int number = rnd.Next( 100, 1000 );
+Console.WriteLine( "Сгенерированное число {0}", number );
+
+    int a = number/100;
+    int b = number%10;
+
+Console.WriteLine("Результат---> {0}{1}", a , b);
