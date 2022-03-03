f5dcdc64e0fb3b205d0e4664d0e1ed0439505fc7
maksotik <maksotik@rambler.ru>
2022-03-03 19:41:28 +0300

    Задача 3- Вывод третьей цифры числа

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите число- ");
+    string number = Console.ReadLine( );
+    int N = int.Parse( number );
+
+if( N >= 100 )
+    Console.WriteLine("Третья цифра числа {0}- {1}", N, number[2]);
+
+else
+    Console.WriteLine(" Третьей цифры нет! ");
+    
+       
